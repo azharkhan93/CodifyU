63a92d2ea6ff3d88b723da3a7ce7bb3ebf84ea88
azharkhan93 <azharkhan.work101@gmail.com>
2024-09-08 03:00:55 +0530

    new images added

--- utils/richText.ts
+++ utils/richText.ts
@@ -4,17 +4,17 @@ export function extractRichText(blocks: any[]): string[] {
       try {
         switch (block.type) {
           case "heading_1":
-            return `<h1 style="margin-bottom: 20px; font-size: 28px;">${processRichText(block.heading_1.rich_text)}</h1>`;
+            return `<h1 style="font-size: 28px;">${processRichText(block.heading_1.rich_text)}</h1>`;
           case "heading_2":
-            return `<h2 style="margin-bottom: 18px; font-size: 24px; font-weight: bold;">${processRichText(block.heading_2.rich_text)}</h2>`;
+            return `<h2 style="font-size: 24px; font-weight: bold;">${processRichText(block.heading_2.rich_text)}</h2>`;
           case "heading_3":
-            return `<h3 style="margin-bottom: 16px; font-size: 20px; font-weight: bold;">${processRichText(block.heading_3.rich_text)}</h3>`;
+            return `<h3 style="font-size: 20px; font-weight: bold;">${processRichText(block.heading_3.rich_text)}</h3>`;
           case "paragraph":
-            return `<p style="margin-bottom: 14px; font-size: 16px;">${processRichText(block.paragraph.rich_text)}</p>`;
+            return `<p style="font-size: 16px;">${processRichText(block.paragraph.rich_text)}</p>`;
           case "image":
             const imageUrl = block.image?.file?.url || block.image?.external?.url || "";
             return imageUrl
-              ? `<img src="${imageUrl}" alt="Image" style="width: 100%; max-width: 600px; height: auto; margin-bottom: 20px; border-radius: 20px;" />`
+              ? `<img src="${imageUrl}" alt="Image" style=" width: 400px;  border-radius: 20px;" />`
               : "";
           case "embed":
           case "url":
