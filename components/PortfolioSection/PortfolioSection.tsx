874a742c4eefe0b514ac10a01207e10f6d9eabac
azharkhan93 <azharkhan.work101@gmail.com>
2024-09-08 16:32:59 +0530

    porfolio section added

--- components/PortfolioSection/PortfolioSection.tsx
+++ components/PortfolioSection/PortfolioSection.tsx
@@ -20,22 +20,19 @@ export const PortfolioSection: React.FC<PortfolioSectionProps> = ({
         justifyContent={"space-between"}
       >
         <Column
+        py={"xxxl"}
           gap={"xxl"}
           justifyContent={"center"}
           alignItems={"flex-start"}
           width={["100%", "45%"]}
         >
-          <Text variant={["heading", "footerHeading"]}>
-            Why{" "}
-            <span style={{ color: "orange", fontSize: "42px" }}>CodefyU</span>?
+          <Text variant={["footerHeading", "footerHeading"]}>
+            Our{" "}
+            Work
           </Text>
-          <Text variant={"body"}>
-            We have successfully delivered over 200 projects over the last 8
-            years, collectively generating a value of{" "}
-            <span style={{ color: "orange", fontSize: "20px" }}>
-              $200 million
-            </span>
-            . High-Quality tech solutions without high costs.
+          <Text variant={["subHeading","heading"]} textAlign={["start","start"]}>
+          Some Highlights Of Our favorite Projects We Have Done For Forward Thinking Clients.
+            
           </Text>
         </Column>
       </Row>
