84c28d4b0d7bd65dabe8b096f2148859edeb97cb
azharkhan93 <azharkhan.work101@gmail.com>
2024-09-22 15:26:19 +0530

    animations chnged

--- app/template.tsx
+++ app/template.tsx
@@ -1,33 +1,43 @@
 "use client";
 import { Box } from "@/components";
 import { motion } from "framer-motion";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const opacity = {
   initial: { opacity: 0 },
-  enter: { opacity: 1, transition: { duration: 0.5 } },
-  exit: { opacity: 0, transition: { duration: 0.5 } },
+  enter: { opacity: 1, transition: { duration: 0.8 } },
+  exit: { opacity: 0, transition: { duration: 0.8 } },
 };
 
 const expand = {
-  initial: { top: 0 },
-  enter: (i: number) => ({
-    top: "100vh",
-    transition: {
-      duration: 0.8,
-      delay: 0.1 * i,
-      ease: [0.5, 0.05, 0.1, 0.3],
-    },
-    transitionEnd: { height: "0", top: "0" },
-  }),
-  exit: (i: number) => ({
-    height: "100vh",
-    transition: {
-      duration: 0.8,
-      delay: 0.1 * i,
-      ease: [0.5, 0.05, 0.1, 0.3],
-    },
-  }),
+  initial: { top: 0, opacity: 0 },
+  enter: (i: number) => {
+    const isRightSide = i >= 3; 
+    return {
+      top: isRightSide ? "100vh" : "0",
+      opacity: 1,
+      transition: {
+        duration: 1.2,
+        delay: 0.2 * i, 
+        ease: [0.5, 0.05, 0.1, 0.3],
+      },
+      transitionEnd: { height: "0", top: "0", opacity: 0 },
+    };
+  },
+  exit: (i: number) => {
+    const isRightSide = i >= 3; 
+    return {
+      height: "100vh",
+      top: isRightSide ? "0" : "100vh",
+      opacity: 0,
+      transition: {
+        duration: 1.2,
+        delay: 0.2 * i, 
+        ease: [0.5, 0.05, 0.1, 0.3],
+      },
+    };
+  },
 };
 
 const MotionBox = motion(Box);
@@ -43,12 +53,15 @@ export default function Template({ children }: { children: React.ReactNode }) {
   }, [children]);
 
   return (
-    <motion.div
+    <MotionBox
       key={children ? children.toString() : "no-children"}
       initial="initial"
       animate="enter"
       exit="exit"
       variants={opacity}
+      position="relative" 
+      height="100%"
+      width="100%"
     >
       <MotionBox
         flexDirection="row"
@@ -63,35 +76,42 @@ export default function Template({ children }: { children: React.ReactNode }) {
             position="relative"
             height="100%"
             width={`${100 / nbOfColumns}%`} 
-            bg="primary"
+            bg={i < 3 ? "textColor" : "primary"} 
             initial="initial"
             animate="enter"
             exit="exit"
-            variants={expand} 
-            custom={nbOfColumns - i} 
+            variants={expand}
+            custom={i} 
           />
         ))}
       </MotionBox>
-      {showLogo && (
-        <motion.img
-          src="/images/logo.svg" 
-          alt="Logo"
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            zIndex: 100,
-          }}
+      {showLogo ? (
+        <MotionBox
+          position="absolute"
+          top="40%"
+          left="55%"
+          alignItems={"center"}
+          justifyContent={"center"}
+          zIndex={200}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 0.5 } }}
           exit={{ opacity: 0, transition: { duration: 0.5 } }}
-        />
-      )}
+        >
+          <Image 
+            src="/images/logo.svg" 
+            alt="Logo"
+            width={200} 
+            height={200} 
+          />
+        </MotionBox>
+      ): null}
       {children}
-    </motion.div>
+    </MotionBox>
   );
 }
+
+
+
 
 
 
