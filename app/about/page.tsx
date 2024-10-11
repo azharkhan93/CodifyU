4e4b1bdd1e086d2ef36410bd72c50cb605e6eebe
azharkhan93 <azharkhan.work101@gmail.com>
2024-10-11 16:46:13 +0530

    portfolio added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,18 @@
 import { AboutUs, Column, ParallaxSection, WorkApproach } from "@/components";
 
-
 export default function Home() {
   return (
     <>
-     <Column
-    px={["m", "l"]}
-    >
-      <AboutUs />
-      <WorkApproach />
+      <Column px={["m", "l"]}>
+        <AboutUs />
+        <WorkApproach />
       </Column>
       <ParallaxSection
-      spanText = "Chart New Territories"
-          heading="– Let’s Build It with CodefyU!"
-          buttonText="Start Collaborating"
-          animationSrc="https://lottie.host/embed/d11bd612-2169-4a1e-bed7-ad75ecbf7fa5/JtCPghDIUo.json"
-        />
+        spanText="Chart New Territories"
+        heading="– Let’s Build It with CodefyU!"
+        buttonText="Get To Know Us"
+        animationSrc="https://lottie.host/embed/d11bd612-2169-4a1e-bed7-ad75ecbf7fa5/JtCPghDIUo.json"
+      />
     </>
   );
 }
-
