c49cb1cdb010cc5d2105a5153ec30e3a954b8e79
azharkhan93 <azharkhan.work101@gmail.com>
2024-10-24 01:15:59 +0530

    text alligment fixed

--- app/api/sendemail/route.ts
+++ app/api/sendemail/route.ts
@@ -20,17 +20,6 @@ export async function POST(request: Request, response: Response) {
          message: ReceivedResponse.message,
     },
   };
-  // const userData = {
-  //   to: ReceivedResponse.email, 
-  //   from: "azharkhan.work101@gmail.com",
-  //   subject: "Thank You for Contacting Us",
-  //   templateId: "d-your-thank-you-template-id", 
-  //   dynamicTemplateData: {
-  //     name: ReceivedResponse.name,
-  //     lastName: ReceivedResponse.lastName,
-  //     message: "Thank you for reaching out! We appreciate your interest and will get back to you shortly.", 
-  //   },
-  // };
   try {
     await mail.send(data);
     // await mail.send(userData);
