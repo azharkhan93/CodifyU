3ae11bfdb1bc0d1c3b72a0d90116da094ca62627
azharkhan93 <azharkhan.work101@gmail.com>
2024-11-01 12:25:49 +0530

    swal for services added

--- components/ModalContent/ModalContent.tsx
+++ components/ModalContent/ModalContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import axios from "axios";
+import Swal from "sweetalert2";
 import {
   Box,
   Button,
@@ -18,6 +19,7 @@ type FormValues = {
   email: string;
   services: string[];
 };
+
 const FormSchema = Yup.object({
   username: Yup.string().required("Username is required"),
   email: Yup.string()
@@ -34,105 +36,139 @@ export const ModalContent = () => {
       });
 
       if (response.data.sent) {
-        alert("Email sent successfully!");
-      } else {
-        alert("Failed to send email.");
-      }
+        resetForm();
 
-      resetForm();
+        Swal.fire({
+          icon: "success",
+          title:
+            "<span style='color: #fb9c42; font-family: ComfortaaRegular;'>Meeting Scheduled</span>",
+          html: "<p style='color: #fb9c42; font-family: ComfortaaRegular;'>Your meeting request has been successfully sent!</p>",
+          confirmButtonText:
+            "<span style='color: white; font-family: ComfortaaRegular;'>OK</span>",
+          customClass: {
+            popup: "custom-swal-popup",
+            confirmButton: "custom-swal-confirm-button",
+            icon: "custom-swal-icon",
+          },
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: "Failed to send the email. Please try again.",
+        });
+      }
     } catch (error) {
       console.error("Error sending email", error);
-      alert("An error occurred while sending the email.");
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "An error occurred while sending the email.",
+      });
     }
   };
 
   return (
-    <CenterBox width={["100%", "90%"]} height={"100%"} p={"m"}>
-      <Formik
-        initialValues={{
-          username: "",
-          email: "",
-          services: [],
-        }}
-        validationSchema={FormSchema}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
-          handleSubmit(values, resetForm);
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <Box
-              width={"100%"}
-              height={"100%"}
-              alignItems={"center"}
-              textAlign={"start"}
-            >
-              <Column
-                width={["98%", "100%"]}
+    <>
+      <style jsx global>{`
+        .custom-swal-popup {
+          background-color: #f0f0f5 !important;
+          color: #fb9c42 !important;
+        }
+        .custom-swal-icon {
+          background: #fb9c42 !important;
+          color: #fb9c42 !important;
+        }
+        .custom-swal-confirm-button {
+          background-color: #fb9c42 !important;
+          color: black !important;
+          border: none !important;
+          border-radius: 4px !important;
+        }
+      `}</style>
+
+      <CenterBox width={["100%", "90%"]} height={"100%"} p={"m"}>
+        <Formik
+          initialValues={{
+            username: "",
+            email: "",
+            services: [],
+          }}
+          validationSchema={FormSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            handleSubmit(values, resetForm);
+            setSubmitting(false);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form style={{ width: "100%", height: "100%" }}>
+              <Box
+                width={"100%"}
                 height={"100%"}
                 alignItems={"center"}
-                justifyContent={"center"}
-                gap={["xl", "xxl"]}
+                textAlign={"start"}
               >
-                <Text
-                  width={["100%", "450px"]}
-                  textAlign={"center"}
-                  variant={["subHeading", "heading"]}
-                >
-                  {`Let's Level Up Your Brand Together`}
-                </Text>
-                <Box width={["97%", "80%"]}>
-                  <UpdateForm
-                    name={"username"}
-                    placeholder={"Enter Username"}
-                    label={"Username"}
-                    type="text"
-                  />
-                </Box>
-                <Box width={["97%", "80%"]}>
-                  <UpdateForm
-                    name={"email"}
-                    placeholder={"Enter Email"}
-                    label={"Email"}
-                    type="email"
-                  />
-                </Box>
-                <Box width={["97%", "80%"]}>
-                  <FormSearchableSelect
-                    placeholder={"Choose Service"}
-                    label={"Select Service"}
-                    name={"services"}
-                  />
-                </Box>
-
-                <Box
-                  width={["100%", "80%"]}
-                  alignItems={"flex-end"}
+                <Column
+                  width={["98%", "100%"]}
+                  height={"100%"}
+                  alignItems={"center"}
                   justifyContent={"center"}
+                  gap={["xl", "xxl"]}
                 >
-                  <Button
-                    borderRadius={"s"}
-                    px={"xxxl"}
-                    py={"s"}
-                    bg={"primary"}
-                    variant={"primary"}
-                    type="submit"
-                    disabled={isSubmitting}
+                  <Text
+                    width={["100%", "450px"]}
+                    textAlign={"center"}
+                    variant={["subHeading", "heading"]}
                   >
-                    Schedule Meeting
-                  </Button>
-                </Box>
-              </Column>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </CenterBox>
+                    {`Let's Level Up Your Brand Together`}
+                  </Text>
+                  <Box width={["97%", "80%"]}>
+                    <UpdateForm
+                      name={"username"}
+                      placeholder={"Enter Username"}
+                      label={"Username"}
+                      type="text"
+                    />
+                  </Box>
+                  <Box width={["97%", "80%"]}>
+                    <UpdateForm
+                      name={"email"}
+                      placeholder={"Enter Email"}
+                      label={"Email"}
+                      type="email"
+                    />
+                  </Box>
+                  <Box width={["97%", "80%"]}>
+                    <FormSearchableSelect
+                      placeholder={"Choose Service"}
+                      label={"Select Service"}
+                      name={"services"}
+                    />
+                  </Box>
+
+                  <Box
+                    width={["100%", "80%"]}
+                    alignItems={"flex-end"}
+                    justifyContent={"center"}
+                  >
+                    <Button
+                      borderRadius={"s"}
+                      px={"xxxl"}
+                      py={"s"}
+                      bg={"primary"}
+                      variant={"primary"}
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      Schedule Meeting
+                    </Button>
+                  </Box>
+                </Column>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </CenterBox>
+    </>
   );
 };
