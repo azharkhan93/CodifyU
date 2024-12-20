becd84d53293e55e9e1e3dbf43b26cc2d0dd4589
azharkhan93 <azharkhan.work101@gmail.com>
2024-12-21 00:45:45 +0530

    blog cards added

--- components/BlogSection/components/PostCards/PostCards.tsx
+++ components/BlogSection/components/PostCards/PostCards.tsx
@@ -17,24 +17,22 @@ export const PostCards: React.FC<PostCardProps> = ({ post, loading }) => {
     return <LoadingCard />;
   }
 
-  
   const formattedDate = new Date(post.date);
   const year = formattedDate.getFullYear();
-  const month = (formattedDate.getMonth() + 1).toString().padStart(2, "0"); 
-  const day = formattedDate.getDate().toString().padStart(2, "0"); 
+  const month = (formattedDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = formattedDate.getDate().toString().padStart(2, "0");
   const formattedDateStr = `${year}-${month}-${day}`;
 
   return (
     <Column
-    height={'520px'}
-    border={"1px solid #fb9c42"}
-    borderRadius={"s"}
+      height={["auto", "485px"]}
+      border={"1px solid #fb9c42"}
+      borderRadius={"s"}
       width={["350px", "370px"]}
-      
       overflow="hidden"
       position="relative"
       style={{
-        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", 
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
       }}
     >
       {post.file ? (
@@ -54,17 +52,33 @@ export const PostCards: React.FC<PostCardProps> = ({ post, loading }) => {
           }}
         />
       )}
-      <Column alignItems="flex-start" gap="l" px="m" py="xxxl" >
-        <Text variant="subHeading" width={["100%", "100%"]}  style={{
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
-    overflow: "hidden"
-  }}>{post.title || "Untitled Post"}</Text>
-        <Text variant="body" color="secondary" width={["100%", "100%"]} lineHeight={"1.5"} >
+      <Column alignItems="flex-start" gap="l" px="l" py="xxxl">
+        <Text
+          variant="subHeading"
+          width={["100%", "100%"]}
+          style={{
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+          }}
+        >
+          {post.title || "Untitled Post"}
+        </Text>
+        <Text
+          variant="body"
+          color="secondary"
+          width={["100%", "100%"]}
+          lineHeight={"1.5"}
+        >
           {post.description || "No description available."}
         </Text>
 
-        <Row alignItems="center" gap="xl" flexDirection="row" justifyContent={"center"}>
+        <Row
+          alignItems="center"
+          gap="xl"
+          flexDirection="row"
+          justifyContent={"center"}
+        >
           <FaUserAlt size={19} />
           <Text variant="body" color="primary">
             {post.author || "Unknown Author"}
@@ -72,40 +86,32 @@ export const PostCards: React.FC<PostCardProps> = ({ post, loading }) => {
         </Row>
 
         <Text
-        variant="body"
-        color="white"
-        bg={"Overlay"}
-        py={"s"}
-        textAlign={"center"}
-        borderRadius={"xs"}
-        position="absolute"
-        top={20} 
-        left={5}
-        width="32%" 
-        zIndex={1} 
-      >
-        {formattedDateStr}
-      </Text>
-
-      <CenterBox
-      alignSelf={"flex-end"}
-      >
-        <Button 
-        bg={"textColor"}
-        variant="primary"
-        borderRadius={"xs"}
-        py={"s"}
+          variant="body"
+          color="white"
+          bg={"Overlay"}
+          py={"s"}
+          textAlign={"center"}
+          borderRadius={"xs"}
+          position="absolute"
+          top={20}
+          left={5}
+          width="32%"
+          zIndex={1}
         >
-          Read More
+          {formattedDateStr}
+        </Text>
 
-        </Button>
-
-      </CenterBox>
+        <CenterBox alignSelf={"flex-end"} px={"xl"}>
+          <Button
+            bg={"textColor"}
+            variant="primary"
+            borderRadius={"xs"}
+            py={"s"}
+          >
+            Read More
+          </Button>
+        </CenterBox>
       </Column>
     </Column>
   );
 };
-
-
-
-
