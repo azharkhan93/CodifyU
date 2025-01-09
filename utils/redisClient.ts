b20191d9c1c303a5b6547f547f4c2d4232c9bfbf
azharkhan93 <azharkhan.work101@gmail.com>
2025-01-09 23:27:39 +0530

    new chahnges

--- utils/redisClient.ts
+++ utils/redisClient.ts
@@ -1,12 +1,13 @@
 import Redis from 'ioredis';
 
 export const client = new Redis({
-    username: process.env.REDIS_USERNAME,
+    username: process.env.REDIS_USERNAME || undefined,
     password: process.env.REDIS_PASSWORD,
     host: process.env.REDIS_HOST,  
-    port: Number(process.env.REDIS_PORT) || 6379,tls: {
-        rejectUnauthorized: false, 
-      },
+    port: Number(process.env.REDIS_PORT) || 6379,
+    maxRetriesPerRequest: 5, 
+    enableOfflineQueue: true,
+    
     
 });
 
