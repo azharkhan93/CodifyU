2e928c66614b560e10a74ad3b9eeb49dd61a8fe6
azharkhan93 <azharkhan.work101@gmail.com>
2025-01-10 01:48:27 +0530

    env added

--- utils/redisClient.ts
+++ utils/redisClient.ts
@@ -1,25 +1,25 @@
-import Redis from 'ioredis';
+// import Redis from 'ioredis';
 
 
 
-export const client = new Redis({
-    username: process.env.REDIS_USERNAME || undefined,
-    password: process.env.REDIS_PASSWORD,
-    host: process.env.REDIS_URL,  
-    port: Number(process.env.REDIS_PORT) || 6379,
-    maxRetriesPerRequest: 5, 
-    enableOfflineQueue: true,    
+// export const client = new Redis({
+//     username: process.env.REDIS_USERNAME || undefined,
+//     password: process.env.REDIS_PASSWORD,
+//     host: process.env.REDIS_URL,  
+//     port: Number(process.env.REDIS_PORT) || 6379,
+//     maxRetriesPerRequest: 5, 
+//     enableOfflineQueue: true,    
     
-});
+// });
 
-client.on('error', (err: any) => {
-    console.error('Redis Client Error', err);
-});
+// client.on('error', (err: any) => {
+//     console.error('Redis Client Error', err);
+// });
 
 
-client.on('connect', () => {
-    console.log('Connected to Redis!');
-});
+// client.on('connect', () => {
+//     console.log('Connected to Redis!');
+// });
 
 
 
