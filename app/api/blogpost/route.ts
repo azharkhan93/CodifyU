468e132b3f4c74dfc57a8ad53845a00499f6ea76
azharkhan93 <azharkhan.work101@gmail.com>
2025-01-10 16:01:37 +0530

    new cahnges

--- app/api/blogpost/route.ts
+++ app/api/blogpost/route.ts
@@ -1,54 +1,28 @@
 import { NextResponse } from "next/server";
 import { getBlogPosts } from "@/utils/notion";
-import { getRedisClient } from "@/utils/redisClient";
-
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const slug = url.searchParams.get("slug");
 
-  const redisClient = getRedisClient(); 
-  const allPostsCacheKey = "blogPosts";
-
   try {
-    
-    const cachedAllPosts = await redisClient.get(allPostsCacheKey);
-
     let posts: any[] = [];
-    if (cachedAllPosts) {
-      console.log("Returning cached data for all posts");
-      posts = JSON.parse(cachedAllPosts);
-    } else {
-      console.log("Fetching new data from Notion for all posts");
-      posts = await getBlogPosts();
-      await redisClient.set(allPostsCacheKey, JSON.stringify(posts), "EX", 400); 
-    }
+
+    // Fetch data from Notion (No caching, always fresh data)
+    console.log("Fetching new data from Notion for all posts");
+    posts = await getBlogPosts();
 
     if (slug) {
-      const cachedPost = await redisClient.get(`blogPost:${slug}`);
+      // Find the post by slug
+      const post = posts.find((p) => p.slug === slug);
 
-      if (cachedPost) {
-        console.log("Returning cached data for slug:", slug);
-        return NextResponse.json(JSON.parse(cachedPost));
+      if (post) {
+        return NextResponse.json(post);
       } else {
-        console.log("Fetching new data from Notion for slug:", slug);
-
-        const post = posts.find((p) => p.slug === slug);
-
-        if (post) {
-          await redisClient.set(
-            `blogPost:${slug}`,
-            JSON.stringify(post),
-            "EX",
-            86400
-          );
-          return NextResponse.json(post);
-        } else {
-          return NextResponse.json(
-            { error: "Post not found" },
-            { status: 404 }
-          );
-        }
+        return NextResponse.json(
+          { error: "Post not found" },
+          { status: 404 }
+        );
       }
     }
 
@@ -61,6 +35,7 @@ export async function GET(request: Request) {
     );
   }
 }
+
 
 
 // S4q1ti9z9fkoyquq3zie2t0ixbdmq0lxlusmagzeicuqifvnmbf
